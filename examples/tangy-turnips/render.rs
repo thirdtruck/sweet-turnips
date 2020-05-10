9e6e26c10d2c016487520facfd473cbc2a402417
JC Holder <jc@thirdtruck.org>
2020-05-10 17:52:57 -0400

    Add side borders

--- examples/tangy-turnips/render.rs
+++ examples/tangy-turnips/render.rs
@@ -1,10 +1,17 @@
-use crate::tangy::{EntityKey, World};
+use crate::tangy::{EntityKey, World, GRID_HEIGHT, GRID_WIDTH};
 use sweet_turnips::sprites::{Color, SpriteGrid};
+
+const MAX_X: u8 = GRID_WIDTH - 1;
 
 pub fn sprite_grid_from_world(
     world: &World,
 ) -> SpriteGrid {
     let mut sprite_grid = SpriteGrid::new();
+
+    for y in 0..GRID_HEIGHT {
+        sprite_grid.big_circle_at(0, y);
+        sprite_grid.big_circle_at(MAX_X, y);
+    }
 
     sprite_grid
 }
