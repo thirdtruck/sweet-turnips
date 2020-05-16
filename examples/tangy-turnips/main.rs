437b681b9fb7a5d4856016ad7cf56abbc2a84002
JC Holder <jc@thirdtruck.org>
2020-05-16 15:15:47 -0400

    Extract midi module

--- examples/tangy-turnips/main.rs
+++ examples/tangy-turnips/main.rs
@@ -11,22 +11,17 @@ use sweet_turnips::{Context, GameResult};
 use sweet_turnips::event;
 use sweet_turnips::event::{KeyCode, KeyMods};
 use sweet_turnips::sprites::{Sprites};
-use sweet_turnips::midi;
-use sweet_turnips::midi::{MidiInput};
+use sweet_turnips::midi::{connect_to_midi, MidiReceiver};
 
 use std::convert::From;
 use std::fs;
 use std::path;
-use std::thread;
 use std::sync::mpsc;
-use std::io::{stdin};
 
 const GAME_NAME: &str = "tangy-turnips";
 
 const SPRITE_SCALE: f32 = 4.0;
 const SPRITE_SIZE: f32 = 8.0 * SPRITE_SCALE;
-
-type MidiReceiver = mpsc::Receiver<(u8, u8)>;
 
 struct MainState {
     world: World,
@@ -145,37 +140,7 @@ impl From<WorldConfig> for World {
 pub fn main() -> GameResult {
     let (tx, rx) = mpsc::channel();
 
-    thread::spawn(move || {
-        let mut midi_in = MidiInput::new("midir reading input").expect("Unable to read MIDI inputs");
-        midi_in.ignore(midi::Ignore::None);
-
-        let in_ports = midi_in.ports();
-        let in_port = match in_ports.len() {
-            0 => None,
-            1 => Some(&in_ports[0]),
-            _ => Some(&in_ports[1]),
-        };
-
-        if in_port.is_none() {
-            return;
-        }
-
-        let in_port = in_port.expect("Unable to select a MIDI input");
-
-        let in_port_name = midi_in.port_name(in_port).expect("Unable to fetch fetch MIDI port name");
-
-        println!("\nFound {} MIDI connections", in_ports.len());
-        println!("\nOpening connection to {}", in_port_name);
-
-        let _connection = midi_in.connect(in_port, "midir-read-input", move |_, message, _| {
-            let (key, value) = (message[1], message[2]);
-            tx.send((key, value)).unwrap();
-        }, ()).expect("Unable to open connection to MIDI input");
-
-        // TODO: Find a better way to keep this thread alive
-        let mut input = String::new();
-        stdin().read_line(&mut input).expect("Unable to read input from STDIN");
-    });
+    connect_to_midi(tx);
 
     let resource_dir = path::PathBuf::from("./resources");
 
---
+++ src/midi.rs
@@ -0,0 +1,43 @@
+use midir;
+pub use midir::{Ignore, MidiInput};
+
+use std::io::{stdin};
+use std::sync::mpsc;
+use std::thread;
+
+pub type MidiReceiver = mpsc::Receiver<(u8, u8)>;
+type MidiSender = mpsc::Sender<(u8, u8)>;
+
+pub fn connect_to_midi(tx: MidiSender) {
+    thread::spawn(move || {
+        let mut midi_in = MidiInput::new("midir reading input").expect("Unable to read MIDI inputs");
+        midi_in.ignore(midir::Ignore::None);
+
+        let in_ports = midi_in.ports();
+        let in_port = match in_ports.len() {
+            0 => None,
+            1 => Some(&in_ports[0]),
+            _ => Some(&in_ports[1]),
+        };
+
+        if in_port.is_none() {
+            return;
+        }
+
+        let in_port = in_port.expect("Unable to select a MIDI input");
+
+        let in_port_name = midi_in.port_name(in_port).expect("Unable to fetch fetch MIDI port name");
+
+        println!("\nFound {} MIDI connections", in_ports.len());
+        println!("\nOpening connection to {}", in_port_name);
+
+        let _connection = midi_in.connect(in_port, "midir-read-input", move |_, message, _| {
+            let (key, value) = (message[1], message[2]);
+            tx.send((key, value)).unwrap();
+        }, ()).expect("Unable to open connection to MIDI input");
+
+        // TODO: Find a better way to keep this thread alive
+        let mut input = String::new();
+        stdin().read_line(&mut input).expect("Unable to read input from STDIN");
+    });
+}
