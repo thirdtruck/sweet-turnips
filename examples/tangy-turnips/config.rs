ee6ba7e5fa9f91391190889bd42e2b0ee0004c00
JC Holder <jc@thirdtruck.org>
2020-05-25 00:59:06 -0400

    Refactor examples to use default_game_config_setup

--- examples/tangy-turnips/config.rs
+++ examples/tangy-turnips/config.rs
@@ -1,9 +1,8 @@
 use serde::{Deserialize, Serialize};
-use serde_yaml;
 
-use std::fs;
-use std::fs::File;
 use std::path::PathBuf;
+
+use sweet_turnips::default_game_config_setup;
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct PlayerShipConfig {
@@ -40,18 +39,5 @@ fn example_game_config() -> GameConfig {
 }
 
 pub fn setup_game_config(config_path: PathBuf) -> GameConfig {
-    if !config_path.exists() {
-        let new_file =
-            &File::create(config_path.clone()).expect("Could not create new config file");
-
-        serde_yaml::to_writer(new_file, &example_game_config())
-            .expect("Could not write to new config file");
-    }
-
-    let config_string = fs::read_to_string(config_path).expect("Could not read config file");
-
-    let config: GameConfig =
-        serde_yaml::from_str(&config_string).expect("Could not parse config file");
-
-    config
+    default_game_config_setup(config_path, example_game_config())
 }
